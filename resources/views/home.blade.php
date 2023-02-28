3d186164c0bc270a407105e7f500bf02179b1305
feriwnarta <feriwnarta26@gmail.com>
2023-02-28 16:50:46 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,29 @@
 @extends('templates.app')
 
+
 @section('header')
 
     <div id="header">
         @include('partials.navbar')
         <div class="container">
 
-            <div class="row">
+
+            <div class="row d-flex align-items-center">
                 <div class="col-sm-6">
                     <div class="margin-top-132 headline-sub">
                         <div>Hello i’m CodeCraft from indonesia,</div>
                     </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-sm-6 position-relative">
                     <div class="margin-top-20 headline-title">
                         Igniting Innovation and Driving Results in Software Development.
                     </div>Ï
                 </div>
+
+                <div class="col-sm-6">
+                    <img src="{{asset('img/programming.png')}}" alt="" class="img-fluid">
+                </div>
             </div>
+
+
 
 
 
